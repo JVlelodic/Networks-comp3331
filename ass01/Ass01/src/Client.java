36cbb02943cf7081415b5b3e2abc1c25ac8500f0
Jackie <jzwang2410@gmail.com>
2019-11-07 01:16:45 +1100

    multithreading started

--- ass01/Ass01/src/Client.java
+++ ass01/Ass01/src/Client.java
@@ -7,10 +7,14 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.
 
-public class Client {
+public class Client extends Thread{
 	
-	private static boolean logStatus = false; 
+	private static boolean logStatus = false;
+	private static Socket serverConnect; 
+	private static ReentrantLock syncLock = new ReentrantLock(); 
 	
 	//Prompt user to enter username and password 
 	private static boolean checkLogin(ObjectOutputStream postObject, ObjectInputStream inObject)  {
@@ -22,16 +26,17 @@ public class Client {
 			System.out.println("Please enter password: "); 
 			String password = userInput.readLine();
 			
+			userInput.close();
 //			DataOutputStream postServer = new DataOutputStream(connectionSocket.getOutputStream());
 //			postServer.writeBytes("Header: user/authenticate" + "\n"); 
 //			postServer.writeBytes(username + "\n"); 
 //			postServer.writeBytes(password + "\n");	
 //			postServer.writeBytes("Connection: close\n"); 
 			
-			ArrayList<String> tmp = new ArrayList<>(); 
-			tmp.add(username); 
-			tmp.add(password); 
-			TCPackage content = new TCPackage("user/authenticate", tmp); 
+			ArrayList<String> body = new ArrayList<>(); 
+			body.add(username); 
+			body.add(password); 
+			TCPackage content = new TCPackage("user/authenticate", body); 
 			
 			postObject.writeObject(content);
 					
@@ -65,22 +70,44 @@ public class Client {
 		int serverPort = Integer.parseInt(args[1]); 
 		
 		//Create socket which connects to server
-		Socket serverConnect = new Socket(serverIP, serverPort); 
+		serverConnect = new Socket(serverIP, serverPort); 
 		ObjectOutputStream postObject = new ObjectOutputStream(serverConnect.getOutputStream()); 
 		ObjectInputStream inObject = new ObjectInputStream(serverConnect.getInputStream()); 
 		
 		while(!logStatus) {
 			if(checkLogin(postObject, inObject)) {
 				logStatus = true;
-				System.out.println("logged in"); 
+				System.out.println("logged in");  
 				break; 
 			}
 			System.out.println("try again"); 
-		}	
+		}
 		
+		Client client = new Client();
+		client.start();
+		
+		//take input
+		while(true) {
+			
+		}
 		
 		// close client socket
-		serverConnect.close();
+		
 	} // end of main
+	
+	public void run() {
+		while(true) {
+			syncLock.lock();
+			//read data do something
+			ObjectInputStream serverResponse = new ObjectInputStream(serverConnect.getInputStream());
+			for(TCPackage data = (TCPackage) serverResponse.readObject(); data != null; data = (TCPackage) serverResponse.readObject()) {
+		    	String header = data.getHeader(); 
+		    	System.out.println(header);
+		    	
+			}
+			syncLock.unlock();
+		}
+		
+	}
 
 } // end of class TCPClient
